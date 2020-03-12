d0894bbd6915d33223f0e9d68d690eb4b9a47370
Franz Miltz <franz@miltz.me>
2020-03-12 16:25:37 +0000

    Moved PPS scripts to separate folder

---
+++ SEM2/PPS/scripts/prime_n.py
@@ -0,0 +1,39 @@
+def is_prime(n):
+  if n == 2 or n == 3: return True
+  if n < 2 or n%2 == 0: return False
+  if n < 9: return True
+  if n%3 == 0: return False
+  r = int(n**0.5)
+  f = 5
+  while f <= r:
+    if n%f == 0: return False
+    if n%(f+2) == 0: return False
+    f +=6
+  return True    
+
+def primes_to(n):
+  numbers = [i for i in range(2,n)]
+  index = 0
+  while index < len(numbers):
+    p = numbers[index]
+    q = p 
+    while q <= n:
+      q += p
+      try:
+        numbers.remove(q)
+      except ValueError as _:
+        pass
+    index += 1
+  return numbers
+
+def check_n(ps):
+  n = 1
+  for p in ps:
+    n *= p
+  n += 1 
+  print(n, is_prime(n))
+
+if __name__ == "__main__":
+  p = primes_to(100)
+  for i in range(len(p)):
+    check_n(p[:i])
